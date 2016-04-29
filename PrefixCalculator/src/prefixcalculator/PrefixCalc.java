00dc4d7b7dfb2fc7c222b0ecf25468b7a8fd3545
Zachary Murphy <murphyz@my.wccc.edu>
2016-04-28 20:56:11 -0400

    Working on EventHandnler

--- PrefixCalculator/src/prefixcalculator/PrefixCalc.java
+++ PrefixCalculator/src/prefixcalculator/PrefixCalc.java
@@ -6,19 +6,23 @@
 package prefixcalculator;
 
 import com.sun.corba.se.impl.activation.CommandHandler;
+import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Panel;
 import static java.awt.SystemColor.info;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -29,83 +33,115 @@ import static jdk.nashorn.internal.runtime.JSType.isNumber;
  * @author Zachary Murphy
  */
 public class PrefixCalc extends Application{
+    private TextField displayField; 
+    private TextField resultField;
+    private TextField cmdField;
+    private String pExpr;
    @Override 
-    public void start(Stage primaryStage) {
-        PrefixCalculator one = new PrefixCalculator();
-        
-        
-        //input for user command
-        TextField inputTextField = new TextField();
-        inputTextField.setPrefColumnCount(5);
-        
-        //will give results
-        TextField resultTextField = new TextField();
-        resultTextField.setPrefColumnCount(5);
-        resultTextField.setEditable(false);
-        
-        EventHandler<ActionEvent> handler = 
-                new CommandHandler(one, inputTextField, resultTextField);
-        inputTextField.setOnAction(handler);
-        
-        
-        //vbox to hold fields/labels
-      VBox vBox1 = new VBox(10);
-      Label cmdLabel = new Label("Command: ");
-      Label resultLabel = new Label ("Results: ");
-
-      vBox1.getChildren().addAll(cmdLabel, inputTextField, resultLabel, resultTextField);
+    public void start(Stage primaryStage) throws Exception {
+      //Panel resultPanel = new Panel(new GridLayout(2,2));
+      Label prefixExp = new Label("Prefix Expression");
+      displayField = new TextField();
+      displayField.setEditable(false);
+      
+      Label result = new Label("Result Output: ");
+      resultField = new TextField();
+      resultField.setEditable(false);
       
       
-      //vobx to add to scene
-     VBox vBox = new VBox(30);
-     
-     vBox.getChildren().addAll(vBox1);
-     
+      cmdField = new TextField();
+      EventHandler<ActionEvent> handle = new CmdTextListener();
+      //Panel cmdPanel = new Panel(new GridLayout(1,2));
+      Label cmdLabel = new Label("Enter Your Expression: ");
       
-
+      VBox main = new VBox(20);
+      main.getChildren().addAll(prefixExp, displayField, result, resultField, cmdLabel, cmdField);
       
-      //setting all up
-      primaryStage.setScene(new Scene(vBox));
-      primaryStage.setTitle("Prefix Calculator Tool");
-      primaryStage.setHeight(250);
-      primaryStage.setWidth(300);
-      
+      primaryStage.setScene(new Scene(main));
+      primaryStage.setTitle("My Program");
       primaryStage.show();
       
-         
       
-        
-        
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch();
-    }
+}
     
-    //event handler
-    class CommandHandler implements EventHandler<ActionEvent>{
-        private PrefixCalculator one;
-        private TextField inputTextField;
-        private TextField resultTextField;
-        
-        
-        //constructor for command hendler
-        CommandHandler(PrefixCalculator preone, TextField iField, TextField rField ){
-            one = preone;
-            inputTextField = iField;
-            resultTextField = rField;
-        }
+    //reads prefix input
+      private class CmdTextListener implements EventHandler<ActionEvent>
+    {
         @Override
         public void handle(ActionEvent event) {
-            //THIS IS WHERE I AM STUCK AT WHAT TO DO
+             pExpr = cmdField.getText();
+            Node exprTree = buildExpr(new Scanner(pExpr));
+            int value = eval(exprTree);
+            displayField.setText(pExpr);
+            resultField.setText(String.valueOf(value));
             
+        }
+       
+        //reads tree and reutrns val
+        private int eval(Node tree)
+        {
+            if (tree.left == null && tree.right == null) 
+                return Integer.parseInt(tree.value);
+            else
+            {
+                int leftOp = eval(tree.left);
+                int rightOp = eval(tree.right);
+                if (tree.value.equals("*"))
+                    return leftOp * rightOp;
+                if (tree.value.equals("+"))
+                    return leftOp + rightOp;
+            }           
+            return 0;  // Will never be reached
+        }
+        //is it a number starting?
+        private boolean isNumber(String s)
+        {
+            return Character.isDigit(s.charAt(0));
+        }
+
+
+
+        
+        private class Node
+        {
+            String value;
+            Node left;
+            Node right;
+            Node(String v, Node l, Node r)
+            {
+                value = v;
+                left = l;
+                right = r;
+            }
+        }
+        
+       //builds and reads tree
+        
+        Node buildExpr(Scanner sc)
+        {
+            String s = sc.next();
+            if (isNumber(s))
+                return new Node(s, null, null);
+            else
+            {
+                // s is an operator, so build a non-leaf tree
+                Node leftOp = buildExpr(sc);
+                Node rightOp = buildExpr(sc);
+                return new Node(s, leftOp, rightOp);
+            }            
+        }
+     }
+    public static void main(String[] args){
+        launch();
     }
 
-}
-}
+
+    }  
+
+
+
+
+
 
 
 
