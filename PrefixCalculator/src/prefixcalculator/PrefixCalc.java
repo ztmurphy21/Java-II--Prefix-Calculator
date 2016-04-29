50ee6fde1064d9de7110ccf3d159d0b302d20c29
Zachary Murphy <murphyz@my.wccc.edu>
2016-04-29 18:01:04 -0400

    finished!
    What was happening was I was basically declaring the TextField's twice
    which caused them to be "hidden" so I only declared them once in the
    first place as private and everything worked beautifully.

--- PrefixCalculator/src/prefixcalculator/PrefixCalc.java
+++ PrefixCalculator/src/prefixcalculator/PrefixCalc.java
@@ -1,40 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prefixcalculator;
 
-import com.sun.corba.se.impl.activation.CommandHandler;
-import java.awt.GridLayout;
-import java.awt.Insets;
-import java.awt.Panel;
-import static java.awt.SystemColor.info;
-import java.awt.event.ActionListener;
 import java.util.Scanner;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import static jdk.nashorn.internal.runtime.JSType.isNumber;
+
+
 
 /**
  *
  * @author Zachary Murphy
  */
 public class PrefixCalc extends Application{
-     private TextField displayField; 
+     //private TextField displayField; 
     private TextField resultField;
     private TextField cmdField;
    
@@ -46,15 +31,11 @@ public class PrefixCalc extends Application{
       
         //setting up stuff for Prefix Exp
       Label prefixExp = new Label("Prefix Expression");
-      TextField displayField = new TextField();
-      displayField.setEditable(false);
-      //Prefix Exp HBox
-      HBox display = new HBox();
-      display.getChildren().addAll(prefixExp, displayField);
+    
       
       //result output set up
       Label result = new Label("Result Output: ");
-      TextField resultField = new TextField();
+      resultField = new TextField();
       resultField.setEditable(false);
       
       //result hbox
@@ -64,7 +45,7 @@ public class PrefixCalc extends Application{
       
       
       //input line set up
-      TextField cmdField = new TextField();
+      cmdField = new TextField();
       Label cmdLabel = new Label("Enter Your Expression: ");
        
       
@@ -73,22 +54,24 @@ public class PrefixCalc extends Application{
       HBox cmd = new HBox();
       cmd.getChildren().addAll(cmdLabel, cmdField);
       
+      //button to submit and calculate input
       Button submit = new Button("Calculate");
       
       //main vbox for all hboxes 
       VBox main = new VBox(20);
-      main.getChildren().addAll(display, results, cmd, submit);
+      main.getChildren().addAll( results, cmd, submit);
       EventHandler<ActionEvent> handle = new CmdTextListener();
       submit.setOnAction(handle);
 
       //majic for stage ;)
       primaryStage.setScene(new Scene(main));
-      primaryStage.setTitle("My Program");
+      primaryStage.setTitle("Prefix Calculator");
       primaryStage.show();
       
       
 }
 
+   
     
     //reads prefix input
       private class CmdTextListener implements EventHandler<ActionEvent>
@@ -103,8 +86,8 @@ public class PrefixCalc extends Application{
             String pExpr = cmdField.getText();
             Node exprTree = buildExpr(new Scanner(pExpr));
             int value = eval(exprTree);
-           displayField.setText(pExpr);
-            resultField.setText(String.valueOf(value));
+           //displayField.setText(pExpr);
+          resultField.setText(String.valueOf(value));
             
         }
        
